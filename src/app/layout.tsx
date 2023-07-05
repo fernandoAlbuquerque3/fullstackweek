5d95634d05c94488f9112db6206c58beab06a2cd
fernando <fernandonetodeveloper@gmail.com>
2023-07-05 17:34:46 -0300

    chore: add domains to next config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import { NextAuthProvider } from "@/providers/auth"
 import "./globals.css"
 import { Poppins } from "next/font/google"
+import Header from "../components/Header"
+
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -21,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={poppins.className}>
         <NextAuthProvider>
+          <Header />
           {children}
         </NextAuthProvider>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client"
 
+import TripSearch from "./components/TripSearch"
 
 export default function Home() {
   return (
     <div>
-      hello word
+      <TripSearch />
     </div>
   )
 }
